775d8c97acc66a7d670cec233c349638b6f75a01
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-10-20 11:51:36 +0200

    configuration done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
+import './styles/main.scss';
+
 const state = {
   title: 'CI/CD Gallery',
   logo: 'logo-img',
   search: 'Search',
-}
+};
 
-const template = state => {
-  return `
+const template = state => `
     <header>${state.logo}</header>
     <input>${state.search}</input>
     <img src="https://picsum.photos/id/237/200/300">
   `;
-}
 
 const render = (htmlString, el) => {
   el.innerHTML = htmlString;
-}
+};
 
 window.addEventListener('statechange', () => {
   render(template(state), document.querySelector('#root'));
-  render(`<title>PepperShaking Image Gallery</title>`, document.querySelector('head'))
 });
 
 window.dispatchEvent(new Event('statechange'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
@@ -10,6 +23,8 @@ module.exports = {
     filename: '[name].bundle.js',
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'webpack Boilerplate',
       template: path.resolve(__dirname, './src/template.html'), // template file
@@ -18,18 +33,23 @@ module.exports = {
   ],
   module: {
     rules: [
-      // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
-      // Images
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
     ],
   },
-}
-// hdajkshfas
+};
