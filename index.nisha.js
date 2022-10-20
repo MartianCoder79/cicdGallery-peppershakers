d8b970c6b3b44c00c552f2efa2fcd4981cec0379
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-10-20 15:24:16 +0200

    working on a search history

---
+++ index.nisha.js
@@ -0,0 +1,115 @@
+import './styles/main.scss';
+
+const apiKey = 'ADD YOUR OWN API KEY';
+
+const navTag = document.createElement('nav');
+
+
+const form = document.querySelector('.js-form');
+// form.addEventListener('submit', handleSubmit);
+const nextBtn = document.querySelector('.js-next');
+const prevBtn = document.querySelector('.js-prev');
+const resultStats = document.querySelector('.js-result-stats');
+const spinner = document.querySelector('.js-spinner');
+
+let totalResults;
+let currentPage = 1;
+let searchQuery;
+// LocalStorage implementation
+localStorage.setItem('searchedItem', 'computer');
+// Get the existing data
+let existing = localStorage.getItem('searchedItem');
+// If no existing data, create an array
+// Otherwise, convert the localStorage string to an array
+existing = existing ? existing.split(',') : [];
+// Add new data to localStorage Array
+existing.push('tuna');
+// Save back to localStorage
+localStorage.setItem('searchedItem', existing.toString());
+function pagination(totalPages) {
+  nextBtn.classList.remove('hidden');
+  if (currentPage >= totalPages) {
+    nextBtn.classList.add('hidden');
+  }
+  prevBtn.classList.add('hidden');
+  if (currentPage !== 1) {
+    prevBtn.classList.remove('hidden');
+  }
+}
+async function searchUnsplash(searchQry) {
+  const endpoint = `https://api.unsplash.com/search/photos?query=${searchQry}&per_page=30&page=${currentPage}&client_id=${apiKey}`;
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const json = await response.json();
+  return json;
+}
+function displayResults(json) {
+  const searchResults = document.querySelector('.search-results');
+  searchResults.textContent = '';
+  json.results.forEach(result => {
+    const url = result.urls.small;
+    const unsplashLink = result.links.html;
+    const photographer = result.user.name;
+    const photographerPage = result.user.links.html;
+    searchResults.insertAdjacentHTML(
+      'beforeend',
+      `<div>
+        <a href="${unsplashLink}" target="_blank">
+          <div class="result-item" style="background-image: url(${url});"></div>
+        </a>
+        <p class="photographer-name">
+          <a href="${photographerPage}" target="_blank" style="color: black; text-decoration: none;">Photo by ${photographer}</a>
+        </p>
+      </div>`,
+    );
+  });
+  totalResults = json.total;
+  resultStats.textContent = `About ${totalResults} results found`;
+}
+async function fetchResults(srchQuery) {
+  spinner.classList.remove('hidden');
+  try {
+    const results = await searchUnsplash(srchQuery);
+    pagination(results.total_pages);
+    console.log(results);
+    displayResults(results);
+  } catch (err) {
+    // console.log(err);
+    // alert('Failed to search Unsplash');
+  }
+  spinner.classList.add('hidden');
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  currentPage = 1;
+  const inputValue = document.querySelector('.js-search-input').value;
+  searchQuery = inputValue.trim();
+  console.log(searchQuery);
+  fetchResults(searchQuery);
+}
+nextBtn.addEventListener('click', () => {
+  currentPage += 1;
+  fetchResults(searchQuery);
+});
+prevBtn.addEventListener('click', () => {
+  currentPage -= 1;
+  fetchResults(searchQuery);
+});
+form.addEventListener('submit', handleSubmit);
+// Use HTML5 Local Storage to store and read saved searches.:-------------------
+localStorage.setItem('myKey', 'myValue');
+window.addEventListener('storage', event => {
+  console.log(`The value for ${event.key} was changed from${event.oldValue} to ${event.newValue}`);
+}, false);
+// Check for HTML5 Storage..
+function supportsHtml5Storage() {
+  try {
+    return 'localStorage' in window && window.localStorage !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
+supportsHtml5Storage();
--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import './styles/main.scss';
 
 const state = {
   title: 'CI/CD Gallery',
-  logo: 'logo-img',
-  search: 'Search',
+  logo: 'PepperShaking Image Gallery',
+  logoImg: '../public/shaker.png',
 };
 
-const template = state => `
-    <header>${state.logo}</header>
-    <input>${state.search}</input>
-    <img src="https://picsum.photos/id/237/200/300">
+const template = content => `
+    <header class="header">
+      <nav class="nav">
+        <img class="logo" src="aabd65e9e8cc266db58c.png" />
+          ${content.logo}
+        <img class="logo" src="aabd65e9e8cc266db58c.png" />
+      </nav>
+    </header>
+    <main class="main">
+      <section class="search">
+        <input type="text" placeholder="Search.." class="input" id="search__item">
+        <button type="submit" class="btn" id="go">GO</button>
+      </section>
+    </main>
   `;
 
 const render = (htmlString, el) => {
@@ -21,3 +31,16 @@ window.addEventListener('statechange', () => {
 });
 
 window.dispatchEvent(new Event('statechange'));
+
+const search = document.querySelector('#search__item');
+const go = document.querySelector('#go');
+
+const searchHistoryArray = ['tuna', 'computer', 'man'];
+
+window.localStorage.setItem('searchHistory', JSON.stringify(searchHistoryArray));
+
+go.addEventListener('click', () => {
+  searchHistoryArray.push(search.value);
+  console.log(searchHistoryArray);
+});
+
